6ecfc98c819622535bc3e61da84cb890c7afda15
Debby95 <debbyjacksonstar@hotmail.it>
2022-12-02 14:47:59 +0100

    update e delete

--- la-mia-pizzeria-static/Controllers/PizzaController.cs
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -45,5 +45,45 @@ namespace la_mia_pizzeria_static.Controllers
             
         }
 
+        public IActionResult Update(int id)
+        {
+            Pizza pizza = db.Pizzas.Where(p => p.Id == id).FirstOrDefault();
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+                
+            return View(pizza);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Pizza pizza)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            db.Pizzas.Update(pizza);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            Pizza pizza = db.Pizzas.Where(p => p.Id == id).FirstOrDefault();
+
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+            db.Pizzas.Remove(pizza);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
